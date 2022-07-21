251ffa6b3b9ac1ceb0ee735767cf361281529829
Carlos Nava <44716371+coldplayer2@users.noreply.github.com>
2022-07-20 21:53:30 -0300

    Implemento de nodeJs y Angular
    Implementación de nodeJs y Angular

---
+++ ProyectoIntegrador_CarlosNava/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
